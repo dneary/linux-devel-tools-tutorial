9765e6a04b2957fa3c3f2678cfc64db04b9f52a1
Dave Neary <dneary@gnome.org>
2011-12-01 14:09:47 +0100

    Add testfun() function stub

--- demo-gdb.c
+++ demo-gdb.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int testfun(void)
+{
+  /* Useless comment */
+  return 0;
+}
+
 int main(void)
 {
     char buf[80];
+    testfun();
     printf("Please enter some text.\n");
 
     fgets(buf, 80, stdin);
